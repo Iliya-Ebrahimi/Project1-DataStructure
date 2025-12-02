f3e11894970469a267786d44181d931077209ced
Iliya-Ebrahimi <iliyaebrahimiwork1000@gmail.com>
2025-12-02 18:15:45 +0330

    Build Tree and algorithms of parse of operators and numbers
    add functions:
    isOperator
    prioritizing_operators
    right_operator_effect
    parser_add_sub
    parser_mult_div
    parser_power_Radikal
    var_OR_bracket
    build_tree

--- main.cpp
+++ main.cpp
@@ -9,29 +9,6 @@
 #include <string>
 using namespace std;
 
-// Tree Nodes
-struct TreeNode {
-    string value;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(const string& v) : value(v), left(nullptr), right(nullptr) {}
-    ~TreeNode() {
-        delete left;
-        delete right;
-    }
-};
-
-
-// throw an error if the number under a sqrt is negatave !( does not work if the answer under it is negative --> √5-6 )!
-void NegativeSqrt(string s){
-    for (int i = 0; i< s.size()-1;i++){
-        if (s[i] == '√' and s[i+1] == '-'){
-            throw runtime_error("No negative sqrts");
-        }
-    }
-}
-
-
 // Normalizer --> Correct user input
 string normalizer(string expr) {
     auto isSign = [](char c) {
@@ -112,6 +89,7 @@ string normalizer(string expr) {
 vector<string> tokenize(const string& expr) {
     vector<string> tokens;
     string token;
+    tokens.push_back("(");
     for (int i = 0; i < expr.size(); ++i) {
         char c = expr[i];
         if (isspace(c)) continue;
@@ -139,8 +117,90 @@ vector<string> tokenize(const string& expr) {
             throw runtime_error("Invalid character: " + string(1, c)); // error for invalid char like # @
         }
     }
+    tokens.push_back(")");
     return tokens; // return a vector for use in tree  
 }
+
+//////////////// Functions for build tree /////////////////
+
+// function for: "This token is operator?" 
+bool isOperator (const string& s) {
+    return (s == "+" || s == "-" || s == "*" || s == "/" || s == "^" || s == "√");
+}
+
+// Prioritizing operators
+int prioritizing_operators (const string& s) {
+    if (s == "+" || s == "-") return 1;
+    if (s == "*" || s == "/") return 2;
+    if (s == "^") return 3;
+    return 0;
+}
+
+// Right-effect operator detection
+bool right_operator_effect (const string& s) {
+    return (s == "^" || s == "√");
+}
+
+// throw an error if the number under a sqrt is negatave !( does not work if the answer under it is negative --> √5-6 )!
+void NegativeSqrt(string s){
+    for (int i = 0; i< s.size()-1;i++){
+        if (s[i] == '√' and s[i+1] == '-'){
+            throw runtime_error("No negative sqrts");
+        }
+    }
+}
+
+
+// Tree Nodes
+struct TreeNode {
+    string value;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(const string& v) : value(v), left(nullptr), right(nullptr) {}
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
+};
+
+
+TreeNode* parser_add_sub (const vector<string>& tokens, int& index){
+    // parser add Or sub and numbers of tokens
+}
+
+TreeNode* parser_mult_div (const vector<string>& tokens, int& index){
+    // parser mult Or div and numbers of tokens
+}
+
+TreeNode* parser_power_Radikal (const vector<string>& tokens, int& index){
+    // parser power Or Radikal and number of tokens
+}
+
+TreeNode* var_OR_bracket (const vector<string>& tokens, int& index) {
+    string tok = tokens[index];
+    if (isdigit(tok[0])) {
+        // ...
+    }
+    else if (isalpha(tok[0])) {
+        // ...
+    }
+    else if (tok == "(") {
+        // ...
+    }
+    else {
+        // for operators...
+    }
+}
+
+TreeNode* build_tree (const vector<string>& tokens, int& index) {
+    int index = 0;
+    TreeNode* root = var_OR_bracket(tokens, index);
+    if (index != tokens.size()) {
+        throw runtime_error("Unexpected token after parsing expression");
+    }
+    return root;
+}
+
 
 int main(){
     string user_input;
